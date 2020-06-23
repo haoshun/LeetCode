35c298d84681b0e71c772269114e1fbe42eec5b1
Yuanshun Hao <haoshun_cool@sina.com>
2020-06-23 20:16:20 +0800

    【工程调整】	添加细分工程

---
+++ Trees/Trees/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Trees
+//
+//  Created by 郝源顺 on 2020/6/23.
+//  Copyright © 2020 郝源顺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
